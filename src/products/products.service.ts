2ba2be61b0a07f54540bf9f8285d305b4a5080cd
aquispesegales <35657468+aquispesegales@users.noreply.github.com>
2024-11-09 23:02:33 -0400

    completado el video 4.9 de la ruta I:\CURSO\NEST MICRO SERVICIO fernando\4.Products Microservice\4.Products Microservice

---
+++ src/products/products.service.ts
@@ -0,0 +1,52 @@
+import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
+import { CreateProductDto } from './dto/create-product.dto';
+import { UpdateProductDto } from './dto/update-product.dto';
+import { PrismaClient } from '@prisma/client';
+import { PaginationDto } from 'src/common';
+
+@Injectable()
+export class ProductsService extends PrismaClient implements OnModuleInit {
+
+  private readonly logger = new Logger('ProductsService');
+
+  onModuleInit() {
+    this.$connect();
+    this.logger.log('Database conected');
+    //throw new Error('Method not implemented.');
+  }
+  create(createProductDto: CreateProductDto) {
+
+    return this.product.create({
+      data:createProductDto
+    })
+  }
+
+  async findAll(paginationDto:PaginationDto) {
+    const {page,limit} =  paginationDto;
+    const totalPages = await this.product.count();
+    const lastPage = Math.ceil(totalPages/limit);
+    return {
+      data:await this.product.findMany({
+        skip:(page-1)*limit,
+        take:limit
+      }),
+      meta:{
+        total:totalPages,
+        page:page,
+        lastPage:lastPage
+      }
+    };
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} product`;
+  }
+
+  update(id: number, updateProductDto: UpdateProductDto) {
+    return `This action updates a #${id} product`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} product`;
+  }
+}
